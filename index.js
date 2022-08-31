7c22c059ebd2a54341c52faa7d79196604316d36
Jean Campelo <jean93campelo@gmail.com>
2022-08-31 15:41:33 -0300

    Validando se usuário já está cadastrado em base local

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
 const server = express();
 server.use(cors());
 server.use(express.json());
 const port = 5000;
 
+const users = [
+  { name: "nameTest1" },
+  { name: "nameTest2" },
+  { name: "corvus" },
+];
+server.post("/participants", (req, res) => {
+  const { name } = req.body;
 
-server.post('/participants', (req, res) => {
-    const { name } = req.body;
-    
-    if (!name || typeof(name) !== 'string') {
-        return res.send(422);
-    }
-    res.send(201);
-})
+  if (!name || typeof name !== "string") {
+    res.status(422);
+  } 
 
+  const nameExisting = users.find((user) => user.name === name);
+  if (nameExisting) {
+    res.status(409).send({ error: "Nome de usuário já cadastrado" });
+  }
+
+  res.status(201);
+});
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
