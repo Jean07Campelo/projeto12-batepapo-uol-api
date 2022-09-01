733d9b03c62421c2dc7d5cd98d924939f3d76972
Jean Campelo <jean93campelo@gmail.com>
2022-09-01 14:19:06 -0300

    Conectando aplicação com banco de dados

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 import express from "express";
 import cors from "cors";
+import { MongoClient } from "mongodb";
 
 const server = express();
 server.use(cors());
 server.use(express.json());
 const port = 5000;
+const mongoClient = new MongoClient("mongodb://localhost:27017");
+
+let db;
+mongoClient.connect().then(()=>{
+  db = mongoClient.db('test');
+});
 
 const users = [
   { name: "nameTest1" },
@@ -25,5 +32,13 @@ server.post("/participants", (req, res) => {
 
   res.status(201);
 });
+
+//apenas teste de conexão com o banco
+server.get('/participants', (req, res) => {
+  db.collection("dataUOL").find().toArray().then(response => 
+    {
+      res.send(response)
+    });
+})
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
